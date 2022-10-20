034a8a592f651cfe643cdcfd5fba6c41f3095a8c
yonua <2057035911@qq.com>
2022-10-20 19:46:34 +0800

    init commit

---
+++ src/difficult.rs
@@ -0,0 +1,4 @@
+
+pub fn _2246(_parent: Vec<i32>, _s: String) -> i32 {
+    return 0;
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod difficult;
+
+fn main() {
+    println!(
+        "Hello, world {}!",
+        difficult::_2246(vec![1, 2], String::from(""))
+    );
+    println!("{}", 12);
+}
