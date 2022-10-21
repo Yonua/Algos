f38c4767ae35278b566ba676446d151c30db4a10
yonua <2057035911@qq.com>
2022-10-21 20:39:15 +0800

    add 0820

--- src/easy.rs
+++ src/easy.rs
@@ -6,3 +6,11 @@ pub fn _2220(start: i32, goal: i32) -> i32 {
     // 可以发现通过比较二进制对应位数是否相同，完成计算。
     // 直到一个商为0时结束循环，剩余没有结束的继续，结束的为0完成比较。
 }
+
+pub fn _0821(s: String, c: char) -> Vec<i32> {
+    // two for
+    // left -> right , meeting 'c' record index as idx
+    // and calc answer[i] = abs(i - idx)
+    // right -> left , meeting 'c' record index as idx
+    // and calc answer[i] = min(answer[i], abs(i - idx))
+}
